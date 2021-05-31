a0fbb5ff76875327d4f9d81153399f08a0bf3948
Dmitriy Fisina <dmitriy.f@smart-gamma.com>
2021-06-01 00:23:15 +0300

    added link to github repo

--- custom-jsdelivr/src/components/App/Footer.vue
+++ custom-jsdelivr/src/components/App/Footer.vue
@@ -5,7 +5,11 @@
                 <b-col>
                     <div class="m-3 d-flex align-items-center flex-column">
                         <h3>Dmitriy Fisina</h3>
-                        <b-button class="mt-2 bg-gradient" target="_blanc" variant="primary" href=""
+                        <b-button
+                            class="mt-2 bg-gradient"
+                            target="_blanc"
+                            href="https://github.com/dmitriyfisina/vue.js_projects/tree/master/custom-jsdelivr"
+                            variant="primary"
                             >Project GitHub</b-button
                         >
                     </div>
